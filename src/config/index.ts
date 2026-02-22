a882468dd690c7c9d776df0ae9b40772b81ce7c7
mine0059 <onojefemuemine@gmail.com>
2026-02-22 18:34:21 +0100

    add: mongoDB URI and LogLevel config

--- src/config/index.ts
+++ src/config/index.ts
@@ -11,6 +11,8 @@ const config = {
     PORT: process.env.PORT || 3000,
     NODE_ENV: process.env.NODE_ENV,
     WHITELIST_ORIGINS: ['https://docs.blog-api.oghenemine.com'],
+    MONGO_URI: process.env.MONGO_URI,
+    LOG_LEVEL: process.env.LOG_LEVEL || 'info',
 }
 
 export default config;
