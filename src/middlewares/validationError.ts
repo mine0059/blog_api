84f5d02f488a9529811c1ed3529a50209f0bc9ef
mine0059 <onojefemuemine@gmail.com>
2026-02-23 16:00:25 +0100

    add: validation error middleware for auth routes

---
+++ src/middlewares/validationError.ts
@@ -0,0 +1,24 @@
+/**
+ * @copyright 2026 oghenemine emmanuel
+ * @license Apache-2.0
+ */
+
+import { validationResult } from "express-validator";
+
+import type { Request, Response, NextFunction } from "express";
+
+const validationError = (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.status(400).json({
+            code: 'ValidationError',
+            errors: errors.mapped(),
+        });
+        return;
+    }
+
+    next();
+};
+
+export default validationError;
