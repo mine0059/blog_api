ecbe528754549ae7e5bd90f960be503e5f8cd8bd
mine0059 <onojefemuemine@gmail.com>
2026-02-27 12:13:15 +0100

    add: like model for database

---
+++ src/models/like.ts
@@ -0,0 +1,28 @@
+/**
+ * @copyright 2026 oghenemine emmanuel
+ * @license Apache-2.0
+ */
+
+import { Schema, model, Types } from "mongoose";
+
+interface ILike {
+    blogId?: Types.ObjectId;
+    userId: Types.ObjectId;
+    commentId?: Types.ObjectId;
+}
+
+const likeSchema = new Schema<ILike>({
+    blogId: {
+        type: Schema.Types.ObjectId,
+    },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    commentId: {
+        type: Schema.Types.ObjectId,
+    },
+});
+
+export default model<ILike>('Like', likeSchema);
