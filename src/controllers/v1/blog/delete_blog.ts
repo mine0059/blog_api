f93e766f0e7c048ac5dbb1e50a62e960e3a68fa4
mine0059 <onojefemuemine@gmail.com>
2026-02-27 12:10:01 +0100

    add: blog controllers for CRUD operations

---
+++ src/controllers/v1/blog/delete_blog.ts
@@ -0,0 +1,72 @@
+/**
+ * @copyright 2026 oghenemine emmanuel
+ * @license Apache-2.0
+ */
+import { v2 as cloudinary } from 'cloudinary';
+
+import { logger } from "@/lib/winston";
+
+import Blog from "@/models/blog";
+import User from "@/models/user";
+
+import type { NextFunction, Request, Response } from "express";
+
+const deleteBlog = async (req: Request, res: Response, next: NextFunction) : Promise<void> => {
+    try {
+        const userId = req.userId;
+        const blogId = req.params.blogId;
+
+        const user = await User.findById(userId)
+                .select('role')
+                .lean()
+                .exec();
+        const blog = await Blog.findById(blogId)
+                .select('author banner.publicId')
+                .lean()
+                .exec();
+
+        if (!blog) {
+            res.status(404).json({
+                code: 'NotFound',
+                message: 'Blog not found'
+            });
+            return;
+        }
+
+        if (blog.author !== userId && user?.role !== 'admin') {
+            res.status(403).json({
+                code: 'AuthorizationError',
+                message: 'Access denied, insufficient permissions'
+            });
+
+            logger.warn('A user tried to delete a blog without permission', {
+                userId,
+            });
+
+            return;
+        }
+
+        await cloudinary.uploader.destroy(blog.banner.publicId);
+        logger.info('Blog banner deleted from cloudinary', {
+            publicId: blog.banner.publicId,
+        });
+
+        await Blog.deleteOne({ _id: blogId });
+
+        logger.info('Blog deleted successfully', {
+            blogId
+        });
+
+        res.sendStatus(204);
+    } catch (error) {
+        res.status(500).json({
+            code: 'ServerError',
+            message: 'Internal server error',
+            error: error,
+        });
+
+        logger.error('Error during blog deletion', error);
+    }
+}
+
+export default deleteBlog;
---
+++ src/controllers/v1/blog/get_blog_by_user.ts
@@ -0,0 +1,60 @@
+/**
+ * @copyright 2026 oghenemine emmanuel
+ * @license Apache-2.0
+ */
+
+import { logger } from "@/lib/winston";
+import config from "@/config";
+
+import Blog from "@/models/blog";
+import User from "@/models/user";
+
+import type { NextFunction, Request, Response } from "express";
+
+interface QueryType {
+    status?: 'draft' | 'published';
+}
+
+const getBlogsByUser = async (req: Request, res: Response, next: NextFunction) : Promise<void> => {
+    try {
+        const userId = req.params.userId;
+        const currentUserId = req.userId;
+        const limit = parseInt(req.query.limit as string) || config.defaultResLimit;
+        const offset = parseInt(req.query.offset as string) || config.defaultResOffset;
+
+        const currentUser = await User.findById(currentUserId).select('role').lean().exec();
+        const query: QueryType = {};
+
+        // show only the published post to a normal user
+        if (currentUser?.role === 'user') {
+            query.status = 'published';
+        }
+
+         const total = await Blog.countDocuments({ author: userId, ...query });
+        const blogs = await Blog.find({ author: userId, ...query })
+            .select('-banner.publicId -__v')
+            .populate('author', '-createdAt -updatedAt -__v')
+            .limit(limit)
+            .skip(offset)
+            .sort({ createdAt: -1 })
+            .lean()
+            .exec();
+
+        res.status(200).json({
+            limit,
+            offset,
+            total,
+            blogs
+        });
+    } catch (error) {
+        res.status(500).json({
+            code: 'ServerError',
+            message: 'Internal server error',
+            error: error,
+        });
+
+        logger.error('Error while fetching blogs by user', error);
+    }
+}
+
+export default getBlogsByUser;
