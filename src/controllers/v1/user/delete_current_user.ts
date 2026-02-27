34d807dac1ea52280ae3bad8acb69ae40ea1633f
mine0059 <onojefemuemine@gmail.com>
2026-02-27 12:12:22 +0100

    update: delete user controllers to also remove blog and media file after deleting user from db

--- src/controllers/v1/user/delete_current_user.ts
+++ src/controllers/v1/user/delete_current_user.ts
@@ -2,19 +2,38 @@
  * @copyright 2026 oghenemine emmanuel
  * @license Apache-2.0
  */
+import { v2 as cloudinary } from "cloudinary";
 
 import { logger } from "@/lib/winston";
 
 import User from "@/models/user";
+import Blog from "@/models/blog";
 
 import type { NextFunction, Request, Response } from "express";
 
 const deleteCurrentUser = async (req: Request, res: Response, next: NextFunction) : Promise<void> => {
-    const userid = req.userId;
+    const userId = req.userId;
     try {
-        await User.deleteOne({ _id: userid });
+        const blogs = await Blog.find({ author: userId })
+                        .select('banner.publicId')
+                        .lean()
+                        .exec();
+        const publicIds = blogs.map(({ banner }) => banner.publicId );
+        await cloudinary.api.delete_resources(publicIds);
+
+        logger.info('Multiple blog banners deleted from Cloudinary', {
+            publicIds
+        });
+
+        await Blog.deleteMany({ author: userId })
+        logger.info('Multiple blogs deleted', {
+            userId,
+            blogs
+        });
+        
+        await User.deleteOne({ _id: userId });
         logger.info('A user account has been deleted', {
-            userid
+            userId
         });
         res.sendStatus(204);
     } catch (error) {
--- src/controllers/v1/user/delete_user_by_id.ts
+++ src/controllers/v1/user/delete_user_by_id.ts
@@ -2,16 +2,36 @@
  * @copyright 2026 oghenemine emmanuel
  * @license Apache-2.0
  */
+import { v2 as cloudinary } from "cloudinary";
 
 import { logger } from "@/lib/winston";
 
 import User from "@/models/user";
+import Blog from "@/models/blog";
 
 import type { NextFunction, Request, Response } from "express";
 
 const deleteUserById = async (req: Request, res: Response, next: NextFunction) : Promise<void> => {
     try {
         const userId = req.params.userId;
+
+        const blogs = await Blog.find({ author: userId })
+                        .select('banner.publicId')
+                        .lean()
+                        .exec();
+                        
+        const publicIds = blogs.map(({ banner }) => banner.publicId );
+        await cloudinary.api.delete_resources(publicIds);
+
+        logger.info('Multiple blog banners deleted from Cloudinary', {
+            publicIds
+        });
+
+        await Blog.deleteMany({ author: userId })
+        logger.info('Multiple blogs deleted', {
+            userId,
+            blogs
+        });
 
         await User.findByIdAndDelete(userId);
 
