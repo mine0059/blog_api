b50c58a24e67c5c18fc6fb198a7aec1e17cbf821
mine0059 <onojefemuemine@gmail.com>
2026-02-27 12:17:40 +0100

    add: cloudinary .env config

--- src/config/index.ts
+++ src/config/index.ts
@@ -28,6 +28,9 @@ const config = {
     ],
     defaultResLimit: 20,
     defaultResOffset: 0,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME!,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY!,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET!,
 };
 
 export default config;
